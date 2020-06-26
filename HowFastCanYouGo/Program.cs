eee1bfd40bf11ba395b34fb42f50e6f6df787293
Noah Robitshek <noahrobitshek@Noahs-MacBook-Pro-2.local>
2020-06-26 11:23:18 -0400

    first commit

---
+++ HowFastCanYouGo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HowFastCanYouGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
